59f3bb4584f2a6de804d4f98025d1069bb7b56a9
DANILEVICH <danila@MacBook-Pro-Danila.local>
2023-11-25 17:41:20 +0300

    update view model serializers for User_registration

--- backend/views.py
+++ backend/views.py
@@ -27,7 +27,7 @@ class RegisterAccount(APIView):
     def post(self, request):
 
         # проверяем обязательные аргументы
-        if {'first_name', 'last_name', 'email', 'password', 'position'}.issubset(request.data):
+        if {'first_name', 'last_name', 'email', 'password'}.issubset(request.data):
             errors = {}
 
             # проверяем пароль на сложность
